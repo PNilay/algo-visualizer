1e39610d14e9bcb448f4d4f8edda4cc9efc4ff82
npp81 <pnilay926@gmail.com>
2021-02-20 15:01:57 -0500

    added allow diagonals and allow bidirection fields in navInfo model

--- src/app/models/navInfo.ts
+++ src/app/models/navInfo.ts
@@ -1,4 +1,7 @@
 export class NavInfo{
     algorithm:string = '';
     algorithmSpeed!:number;
+    allowBidirection:boolean = false;
+    allowDiagonals:boolean = false;
+    heuristics:'manhattan'| 'euclidean' | 'octile' | 'chebyshev'  = 'manhattan';
 }
