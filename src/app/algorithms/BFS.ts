d804bb9d482573975e0921d8b030853ce89f2daf
npp81 <pnilay926@gmail.com>
2021-02-21 16:52:10 -0500

    bidirectional searching capabalities added

--- src/app/algorithms/BFS.ts
+++ src/app/algorithms/BFS.ts
@@ -8,6 +8,9 @@ export class BFS{
     visited:any[] = [];
     isPathAvail:boolean = false;
     navigation!:NavInfo;
+
+    start_mid:number[] =[];
+    end_mid:number[] =[];
     
 
     constructor(){ 
@@ -16,17 +19,41 @@ export class BFS{
     runBFS(cells:Cell[][], start:number[], end:number[] , navinformation:NavInfo){
         this.gridcells = cells;
         this.navigation = navinformation;
-        this.isPathAvail = this.BFS_Path(start, end);
+
+        if(this.navigation.allowBidirection){
+            this.start_mid = [];
+            this.end_mid = [];
+
+            this.isPathAvail = this.BFS_Bidirection_Path(start, end);
+        }else{
+            this.isPathAvail = this.BFS_Path(start, end);
+        }        
     }
 
     generatePath(start:number[], end:number[]){
         var path = [];
 
-        var prev = this.gridcells[end[0]][end[1]].prev;
-        while(!(prev[0]==start[0] && prev[1]==start[1])){
-            path.push(prev);
-            this.gridcells[prev[0]][prev[1]].vertex_status = 'path';
-            prev = this.gridcells[prev[0]][prev[1]].prev;
+        if(this.navigation.allowBidirection){            
+            var prev = this.start_mid;
+            while(!(prev[0]==start[0] && prev[1]==start[1])){
+                path.push(prev);
+                this.gridcells[prev[0]][prev[1]].vertex_status = 'path';
+                prev = this.gridcells[prev[0]][prev[1]].prev;
+            }
+
+            prev = this.end_mid;
+            while(!(prev[0]==end[0] && prev[1]==end[1])){
+                path.push(prev);
+                this.gridcells[prev[0]][prev[1]].vertex_status = 'path';
+                prev = this.gridcells[prev[0]][prev[1]].prev;
+            }
+        }else{
+            var prev = this.gridcells[end[0]][end[1]].prev;
+            while(!(prev[0]==start[0] && prev[1]==start[1])){
+                path.push(prev);
+                this.gridcells[prev[0]][prev[1]].vertex_status = 'path';
+                prev = this.gridcells[prev[0]][prev[1]].prev;
+            }
         }
         return path;
     }
@@ -68,5 +95,113 @@ export class BFS{
         }
         return false;
 
+    }
+
+    BFS_Bidirection_Path(start:number[], end:number[]){
+        this.visited = [];
+        var q_start = [];
+        q_start.push(start);
+       
+        var q_goal = [];
+        q_goal.push(end);
+
+        while(q_start.length != 0 && q_goal.length != 0){            
+            if(q_start.length != 0){
+                var current = q_start.shift();
+                if(current){
+                    if(this.gridcells[current[0]][current[1]].status == 'end' || this.gridcells[current[0]][current[1]].vertex_status == 'visited_goal'){
+                        return true;
+                    }
+
+                    this.gridcells[current[0]][current[1]].vertex_status = 'current';
+
+                    if(this.navigation.allowDiagonals){
+                        var neighbors = AlgoHelper.findAllNeighbors(current[0], current[1],this.gridcells.length,this.gridcells[0].length);
+                    }else{
+                        var neighbors = AlgoHelper.findNeighbors(current[0], current[1],this.gridcells.length,this.gridcells[0].length);
+                    }
+
+                    while(neighbors.length != 0){
+                        var n = neighbors.shift();
+                        if(n){
+                            if((this.gridcells[n[0]][n[1]].vertex_status == 'unvisited' || this.gridcells[n[0]][n[1]].vertex_status == 'visited_goal' ) && this.gridcells[n[0]][n[1]].status != 'close'){
+                                if(this.gridcells[n[0]][n[1]].vertex_status == 'visited_goal'){
+
+                                    this.visited.push(current);
+                                    this.gridcells[current[0]][current[1]].vertex_status = 'visited_start';
+
+                                    this.start_mid = current;
+                                    this.end_mid = n;
+
+                                    if(this.gridcells[n[0]][n[1]].status != 'start' && this.gridcells[n[0]][n[1]].status != 'end'){
+                                        this.gridcells[n[0]][n[1]].status = 'mid';
+                                        this.gridcells[n[0]][n[1]].vertex_status='unvisited';
+                                    }
+                                    return true;
+                                }
+                                this.gridcells[n[0]][n[1]].vertex_status = 'neighbors';
+                                q_start.push(n);
+                                this.gridcells[n[0]][n[1]].prev = current; 
+                            }                        
+                        } 
+                    } 
+                    this.visited.push(current);
+                    this.gridcells[current[0]][current[1]].vertex_status = 'visited_start'; 
+
+                    // console.log("If visited included in current or not ",  this.visited.includes(current));
+                    
+                }
+            }else{
+                throw new Error('Current node is undefined');
+            }            
+            if(q_goal.length != 0){
+                var current = q_goal.shift();
+                if(current){
+                    if(this.gridcells[current[0]][current[1]].status == 'start' ||  this.gridcells[current[0]][current[1]].vertex_status == 'visited_start'){
+                        return true;
+                    }
+                    
+                    this.gridcells[current[0]][current[1]].vertex_status = 'current';
+
+                    if(this.navigation.allowDiagonals){
+                        var neighbors = AlgoHelper.findAllNeighbors(current[0], current[1],this.gridcells.length,this.gridcells[0].length);
+                    }else{
+                        var neighbors = AlgoHelper.findNeighbors(current[0], current[1],this.gridcells.length,this.gridcells[0].length);
+                    }
+
+                    while(neighbors.length != 0){
+                        var n = neighbors.shift();
+                        if(n){
+                            if((this.gridcells[n[0]][n[1]].vertex_status == 'unvisited' ||  this.gridcells[n[0]][n[1]].vertex_status == 'visited_start' )&& this.gridcells[n[0]][n[1]].status != 'close'){
+                                if(this.gridcells[n[0]][n[1]].vertex_status == 'visited_start'){
+                                    this.visited.push(current);
+                                    this.gridcells[current[0]][current[1]].vertex_status = 'visited_goal'; 
+
+
+                                    this.start_mid = n;
+                                    this.end_mid = current;
+                                    
+                                    if(this.gridcells[n[0]][n[1]].status != 'start' && this.gridcells[n[0]][n[1]].status != 'end'){
+                                        this.gridcells[n[0]][n[1]].status = 'mid';
+                                        this.gridcells[n[0]][n[1]].vertex_status='unvisited';
+                                    }
+                                    
+                                    return true;
+                                }
+                                this.gridcells[n[0]][n[1]].vertex_status = 'neighbors';
+                                q_goal.push(n);
+                                this.gridcells[n[0]][n[1]].prev = current; 
+                            }                        
+                        } 
+                    } 
+                    this.visited.push(current);
+                    this.gridcells[current[0]][current[1]].vertex_status = 'visited_goal'; 
+                }
+            }else{
+                throw new Error('Current node is undefined');
+            }
+        }
+
+        return false;
     }
 }
