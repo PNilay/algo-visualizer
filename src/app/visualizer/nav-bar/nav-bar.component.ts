d7650e5972325d852000749fb0454daadbff6aad
npp81 <pnilay926@gmail.com>
2021-03-21 19:03:15 -0400

    updated navbar and added maze and pattern selection option

--- src/app/visualizer/nav-bar/nav-bar.component.ts
+++ src/app/visualizer/nav-bar/nav-bar.component.ts
@@ -11,12 +11,21 @@ export class NavBarComponent implements OnInit {
   @Output() information: EventEmitter<NavInfo> = new EventEmitter();
   @Output() reset:  EventEmitter<boolean> = new EventEmitter();
   @Output() grid_size:  EventEmitter<number> = new EventEmitter();
+  @Output() weight_values:  EventEmitter<number[]> = new EventEmitter(); //0 index ==> diagonal weight, 1 index ==> toll weight
+  @Output() mazesAndPatterns:EventEmitter<string> = new EventEmitter();
 
   toVis:NavInfo = new NavInfo();
   favoriteColor:number = 5;
   name!: string;
 
+  diagonal_weight:number = 1.42;
+  toll_weight:number = 5;
+
   advance_setting!:boolean;
+
+  maze_pattern:string = "blank";
+
+  custom_grid_size:number = 5;
  
   constructor(){
     this.toVis.algorithm = "Algorithms";
@@ -25,6 +34,7 @@ export class NavBarComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.updateWeights();
   }
 
   handleChange(val:string){
@@ -47,8 +57,19 @@ export class NavBarComponent implements OnInit {
     this.reset.emit(false);
   }
 
+  mazesAndPattern(maze_type:string){
+    this.maze_pattern = maze_type;
+    this.reset.emit(false);
+    this.mazesAndPatterns.emit(this.maze_pattern);
+  }
+
   ResetGridSize(n:number){
     this.grid_size.emit(n);
   }
 
+  updateWeights(){
+    this.weight_values.emit([this.diagonal_weight, this.toll_weight]);
+    // console.log("On focus out", this.diagonal_weight);
+    
+  }
 }
